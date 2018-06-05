489948f191e79902c897bae9a43faafbe0bae1e9
smallyoyo <1421296065@qq.com>
2018-06-05 23:17:49 +0800

    add recommend

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div 
+          class="icon"
+          v-for="item of page"
+          :key="item.id"
+        >
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.img"/>
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      iconList: [{
+        id: '0001',
+        img: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      },
+      {
+        id: '0002',
+        img: 'http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png',
+        desc: '古镇'
+      },
+      {
+        id: '0003',
+        img: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+        desc: '名胜古迹'
+      },
+      {
+        id: '0004',
+        img: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+        desc: '游乐场'
+      },
+      {
+        id: '0005',
+        img: 'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+        desc: '踏青赏花'
+      },
+      {
+        id: '0006',
+        img: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      },
+      {
+        id: '0007',
+        img: 'http://img1.qunarzz.com/piao/fusion/1803/17/99402a22ce4af302.png',
+        desc: '苏州园林'
+      },
+      {
+        id: '0008',
+        img: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+        desc: '拙政园'
+      },
+      {
+        id: '0009',
+        img: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+        desc: '虎丘'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item,index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-container
+    height:0
+    padding-bottom:50%
+    margin-top: .1rem
+  .icon
+      position: relative
+      overflow: hidden
+      float:left
+      width:25%
+      height:0
+      padding-bottom:25%
+      .icon-img
+        position: absolute
+        top:0
+        left:0
+        right:0
+        bottom: .44rem
+        box-sizing:border-box
+        padding: .1rem
+        .icon-img-content
+          display:block
+          margin:0 auto
+          height:100%
+      .icon-desc
+        position: absolute
+        left:0
+        right:0
+        bottom:0
+        height: .44rem
+        line-height: .44rem
+        color:$darkTextColor
+        text-align:center
+        ellipsis()
+</style>
