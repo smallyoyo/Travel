60852b1ed8266400915092513cc8f7dbc33c2dc3
smallyoyo <1421296065@qq.com>
2018-06-06 22:50:32 +0800

    add home

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -12,8 +12,8 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
-import HomeRecommend from "./components/Recommend"
-import HomeWeekend from "./components/Weekend"
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 export default {
   name: 'Home',
   components: {
