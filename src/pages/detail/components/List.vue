e85b717f80eadf7ab6798cd93cc6660aa434d62c
smallyoyo <1421296065@qq.com>
2018-06-29 01:09:14 +0800

    add list

--- src/pages/detail/components/List.vue
+++ src/pages/detail/components/List.vue
@@ -5,6 +5,17 @@
         <span class="item-title-icon"></span>
         {{item.title}}
       </div>
+      <div class="item-content border-bottom" v-for="(contentItem,index) of item.contentList" :key="index">
+        <div class="content-title">
+          {{contentItem.content}}
+        </div>
+        <div class="item-price">
+          ¥
+          <em class="price">{{contentItem.price}}</em>
+          <span class="qi">起</span>
+          <span class="iconfont down">&#xe62d;</span>
+        </div>
+      </div>
       <div v-if="item.children" class="item-children">
         <detail-list :list="item.children"></detail-list>
       </div>
@@ -36,4 +47,23 @@ export default{
       background-size: .4rem 3rem
   .item-children
     padding: 0 .2rem
+  .item-content
+    line-height: .82rem
+    font-size: .30rem
+    padding: 0 .2rem
+    display: flex
+    flex-direction: row
+    flex-wrap: nowrap
+    justify-content: space-between
+    .item-price
+      color: #ff9800
+      font-size: .24rem
+      .price
+        font-size: .42rem
+      .qi
+        font-size: .20rem
+        color: #9e9e9e
+      .down
+        color: #9e9e9e
+        font-size: .30rem
 </style>
